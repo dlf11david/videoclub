e1ca5872bb6fb7954abac5b03ef5c043f8e7c015
davidlopez <dfl11@alu.ua.es>
2023-01-29 22:16:00 +0100

    Método HomeController@getHome

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ class HomeController extends Controller
 {
     public function getHome()
     {
-        return view('home');
+        return redirect()->action([CatalogController::class, 'getIndex']);
+        //return view('home');
     }
 }
