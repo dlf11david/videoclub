d100846905cbac850063907522296da9ad721e8e
davidlopez <dfl11@alu.ua.es>
2023-01-31 23:06:32 +0100

    Método CatalogController@getShow

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
 
-    Vista detalle película {{$id}}
+<div class="row">
+
+<div class="col-sm-4">
+
+    <img src="{{$pelicula['poster']}}" style="height:200px"/>
+
+</div>
+<div class="col-sm-8">
+
+
+    <h3>{{$pelicula['title']}}</h3>
+    <h5>Año: {{$pelicula['year']}}</h5>
+    <h5>Director: {{$pelicula['director']}}</h5>
+    <p><b>Resumen: </b>{{$pelicula['synopsis']}}</p>
+    @if($pelicula['rented'])
+    <p><b>Estado: </b>Película actualmente alquilada</p>
+    <button type="button" class="btn btn-danger">Devolver película</button>
+    <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $id ) }}">Editar película</a>
+    <a class="btn btn-light" href="{{ url('/catalog') }}">Volver al listado</a>
+    @else
+    <p><b>Estado: </b>Película disponible</p>
+    <button type="button" class="btn btn-primary">Alquilar pelicula</button>
+    <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $id ) }}">Editar película</a>
+    <a class="btn btn-light" href="{{ url('/catalog') }}">Volver al listado</a>
+    @endif
+
+</div>
+</div>
 
 @endsection
